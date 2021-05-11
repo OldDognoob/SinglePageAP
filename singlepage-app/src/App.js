a0bf14c5f9b704d585eb87f586fe4b92cffe1157
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-11 19:40:49 +0300

    wip

--- singlepage-app/src/App.js
+++ singlepage-app/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import { Home, About, Contact } from "./pages";
+import { Nav } from "./layout";
 
 function App() {
   return (
-    <div>
-      <h1>Hello People!</h1>
-      <Home/>
-      <About/>
-      <Contact/>
-    </div>
+    <Router>
+      <Nav />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/contact">
+          <Contact />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
--- singlepage-app/src/layout/index.js
+++ singlepage-app/src/layout/index.js
@@ -1,22 +1,11 @@
 import React from "react";
 
+import Nav from './Nav';
+
 const Layout = (props) => (
   <div className="bg-pink-500 border border-black m-5">
-    <nav>
-      <ul>
-        <li>
-          <a href="#">Home</a>
-        </li>
-        <li>
-          <a href="#">About</a>
-        </li>
-        <li>
-          <a href="#">Contact</a>
-        </li>
-      </ul>
-    </nav>
     {props.children}
   </div>
 );
 
-export { Layout };
+export { Layout, Nav };
