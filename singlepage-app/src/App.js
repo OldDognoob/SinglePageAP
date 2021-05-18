9fee36c681fb684ebb6fc51575f8fbb88b13d2c9
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-18 19:29:35 +0300

    wip

--- singlepage-app/src/App.js
+++ singlepage-app/src/App.js
@@ -9,13 +9,13 @@ function App() {
     <Router>
       <Nav />
       <Switch>
-        <Route path="/">
+        <Route exact path="/">
           <Home />
         </Route>
-        <Route path="/about">
+        <Route exact path="/about">
           <About />
         </Route>
-        <Route path="/contact">
+        <Route exact path="/contact">
           <Contact />
         </Route>
       </Switch>
