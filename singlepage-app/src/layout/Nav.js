77f8142dfd47ebd3109d5a0d8c0b4c8c1a7dde20
Dimos-Christidis <christidis.dimosthenis@gmail.com>
2021-05-18 20:52:16 +0300

    wip

--- singlepage-app/src/layout/Nav.js
+++ singlepage-app/src/layout/Nav.js
@@ -1,18 +1,28 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 
+const navs = [{
+  path:'/',
+  name:'Home',
+},
+  {
+  path:'/about', 
+  name:'About',
+},
+  {
+  path:'/contact',
+  name:'Contact',
+},
+]
+
 const Nav = () => (
-  <nav className="bg-purple-600 text-white p-4 shadow">
+  <nav className="bg-purple-600 text-gray-300 p-4 shadow">
     <ul className="flex space-x-6 justify-end">
-      <li>
-        <NavLink exact to="/"activeClassName="border-b-2 border-white">Home</NavLink>
-      </li>
-      <li>
-        <NavLink exact to="/about"activeClassName="border-b-2 border-white">About</NavLink>
-      </li>
-      <li>
-        <NavLink exact to="/contact"activeClassName="border-b-2 border-white">Contact</NavLink>
-      </li>
+      {navs.map(navItem =>(
+        <li>
+          <NavLink exact to={navItem.path}activeClassName='text-gray-100'>{navItem.name}</NavLink>
+        </li>
+      ))}
     </ul>
   </nav>
 );
